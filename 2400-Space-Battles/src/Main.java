66292228d0bd26f2db5aca4d7129b0e8dc0dfbff
craftqq <nimdrop@t-online.de>
2016-06-27 13:23:23 +0200

    +class Main +Method stubs in main

---
+++ 2400-Space-Battles/src/Main.java
@@ -0,0 +1,35 @@
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		//Spiel laden
+		load(args);
+		
+		//Spiel laeuft
+		run(args);
+		
+		//direkt vor Spielende: speichern
+		save(args);
+		System.exit(0);
+	}
+	
+	public static void load(String[] args)
+	{
+		
+		
+	}
+	
+	public static void save(String[] args)
+	{
+		
+		
+	}
+	
+	public static void run(String[] args)
+	{
+		
+		
+	}
+}
