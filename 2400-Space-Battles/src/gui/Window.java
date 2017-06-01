da64a122860498d2ef8ce701c11ad077b81bc39d
craftqq <craftqq@users.noreply.github.com>
2017-06-01 19:13:11 +0200

    Rebase as Maven Project

--- 2400-Space-Battles/src/gui/Window.java
+++ 2400-Space-Battles/src/gui/Window.java
@@ -5,8 +5,14 @@ import javax.swing.JFrame;
 public class Window extends JFrame 
 {
 	
-	public Window()
+	/**
+     * 
+     */
+    private static final long serialVersionUID = 7446192599263749847L;
+
+    public Window()
 	{
 		super("2400 Space Battles");
+		this.setSize(1000, 750);
 	}
 }
