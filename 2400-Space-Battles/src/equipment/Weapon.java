5274bf4ce78a49841d842e526dc2e525f000cc3e
craftqq <craftqq@users.noreply.github.com>
2017-06-01 19:31:08 +0200

    finished class weapon

--- 2400-Space-Battles/src/equipment/Weapon.java
+++ 2400-Space-Battles/src/equipment/Weapon.java
@@ -2,5 +2,32 @@ package equipment;
 
 public class Weapon
 {   
+    private int damage;
+    private int type;
+    public static final int TYPE_LASER = 0;
+    public static final int TYPE_PARTICLE = 1;
+    public static final int TYPE_CONVENTIONAL = 2;
+    public static final int TYPE_ROCKET = 3;
+    public static final int TYPE_TORPEDO = 4;
+    
+    public Weapon(int damage_, int type_)
+    {
+        damage = damage_;
+        if(type<0||type>4)
+        {
+            throw new IllegalArgumentException("the Type has to be set to one of the avaible weapon types");
+        }
+        type = type_;
+    }
+    
+    public int getDamage()
+    {
+        return damage;
+    }
+    
+    public int getType()
+    {
+        return type;
+    }
     
 }
